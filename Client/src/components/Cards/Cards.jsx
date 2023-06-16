d6da642f68b2443c6fda6440434c3cf3cd9b27ff
Carolina Palacios <caaro.palacios@hotmail.com>
2023-06-16 14:53:21 -0300

    proyecto integrador

---
+++ Client/src/components/Cards/Cards.jsx
@@ -0,0 +1,24 @@
+import Card from "../Card/Card";
+import style from "./Cards.module.css";
+
+export default function Cards({ characters, onClose }) {
+  return (
+    <div className={style.container}>
+      {characters.map(
+        ({ id, name, status, species, gender, origin, image }) => {
+          return (
+            <Card
+              key={id}
+              id={id}
+              name={name}
+              species={species}
+              gender={gender}
+              image={image}
+              onClose={onClose}
+            />
+          );
+        }
+      )}
+    </div>
+  );
+}
